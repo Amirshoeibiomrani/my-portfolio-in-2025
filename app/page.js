96997ac5db1b2dfc303046ace75af515b4d9803d
Amir Shoeibi Omrani <amirtahaomrany@gmail.com>
2025-05-27 17:05:17 -0500

    Project , Bio, Skill (Added)

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Projects from './components/Projects'
+import Bio from './components/Bio'
+import Skill from './components/Skill'
 
 const App = () => {
   return (
@@ -9,6 +12,9 @@ const App = () => {
       <div className='relative z-10 flex flex-col items-center p-4 space-y-8 container mx-auto'>
         <Hero/>
         <Navbar/>
+        <Projects/>
+        <Bio/>
+        <Skill/>
       </div>
     </div>
   )
